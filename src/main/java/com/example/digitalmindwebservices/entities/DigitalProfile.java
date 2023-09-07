8445f77eb641c0198e8d0dd7f6728a9d9064f6fa
LenninHuaman <u20201b975@upc.edu.pe>
2023-09-06 21:17:13 -0500

    DatabaseXDigitalProfile: Added digital profile section

--- src/main/java/com/example/digitalmindwebservices/entities/DigitalProfile.java
+++ src/main/java/com/example/digitalmindwebservices/entities/DigitalProfile.java
@@ -1,5 +1,6 @@
 package com.example.digitalmindwebservices.entities;
 
+import com.example.digitalmindwebservices.entities.cross.DatabaseDigitalProfile;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -7,6 +8,7 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.List;
 
 @Entity
 @Table(name = "digital_profiles")
@@ -26,4 +28,7 @@ public class DigitalProfile implements Serializable {
     @JoinColumn(name = "developer_id", nullable = false)
     @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
     private Developer developer;
+
+    @OneToMany(mappedBy = "digitalProfile", cascade = CascadeType.ALL)
+    private List<DatabaseDigitalProfile> databasesDigitalProfile;
 }
