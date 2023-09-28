5ab47fcc279f9e5a3d7e6fe00cae6e5c0058bc81
Shayla Choque <89035883+ShaylaChoque@users.noreply.github.com>
2023-09-27 23:32:02 -0500

    cors updating

--- src/main/java/com/example/digitalmindwebservices/config/WebConfig.java
+++ src/main/java/com/example/digitalmindwebservices/config/WebConfig.java
@@ -1,4 +1,4 @@
-
+package com.example.digitalmindwebservices.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
